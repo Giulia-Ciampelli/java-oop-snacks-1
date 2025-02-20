1da8f540c77f14be2b5e87690fa62a13ba40b785
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2025-02-20 16:42:40 +0100

    docs: delete comments and prints

--- java-oop-snacks-1/src/org/lessons/java/RegistroStudenti.java
+++ java-oop-snacks-1/src/org/lessons/java/RegistroStudenti.java
@@ -1,6 +1,3 @@
-// Snack 3: Registro Studenti
-// Crea una classe RegistroStudenti che rappresenti un registro di studenti. Utilizza un array privato per memorizzare oggetti Studente. Implementa un costruttore senza parametri per inizializzare il registro vuoto e un metodo pubblico per aggiungere studenti al registro. Aggiungi un metodo che stampi la lista degli studenti.
-
 package org.lessons.java;
 
 public class RegistroStudenti {
@@ -21,8 +18,6 @@ public class RegistroStudenti {
     public void printRegistro() {
         for (int i = 0; i < this.studenti.length; i++){
             System.out.println(this.studenti[i].getStudentData()); // non prende il loop, probabilmente perchè devo prima trovare un modo di riempirlo (fill?)
-            System.out.println("array esiste!");
         }
-        System.out.println("metodo esiste!");
     }
 }
